09a8f0f7de62a6bed6acc6a76c7a959a1b22405a
lacava <williamlacava@gmail.com>
2017-08-07 14:23:09 -0400

    updates submit script and FEW_job to test version

--- submit.py
+++ submit.py
@@ -1,34 +1,42 @@
 import os
 import random
 import sys
+import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description='combine csv files in folder into one csv file.',
-                                     add_help=False)
+    parser = argparse.ArgumentParser(description='Submit FEW configuration comaprisons '
+                                     'to the cluster.', add_help=False)
 
     parser.add_argument('-est', type=str, dest='est', help='Comma-separated list of '
                         'python files containing regression model configurations',
-                        default='FEW-lasso,FEW-dtr')
+                        default='dtr,lasso')
+                        # choices = ['lasso','svr','lsvr','rfr','dtr','knr'])
+
     parser.add_argument('-clf', type=str, dest = 'clf', help='Comma-separated list of '
                         'python files containing classification model configurations',
-                        default='FEW-log,FEW-dtc')
-    parser.add_argument('-save_dir', type=str, default='/home/lacava/results/', help='result file')
+                        default='lr,dtc')
+                        # choices = ['lr','svc','rfc','dtc','dc','knc','sgd'])
 
-    parser.add_argument('-h', '--help', action='help', help='Show this help message and exit.')
+    parser.add_argument('-save_dir', type=str, default='/home/lacava/results/',
+                        help='result path')
+
+    parser.add_argument('-h', '--help', action='help',
+                        help='Show this help message and exit.')
 
     parser.add_argument('-reg_probs', action='store', dest='reg_problems', type=str,
                         default='concrete,enc,housing,uball5d,yacht',
-                        type=str, help='Comma-separated list of regression problems.')
-    parser.add_argument('-class_probs', action='store', dest='class_problems', type=str,
+                        help='Comma-separated list of regression problems.')
+
+    parser.add_argument('-clf_probs', action='store', dest='clf_problems', type=str,
                         default='heart,bc_clean,yeast,seg,wav',
-                        type=str, help='Comma-separated list of classification problems.')
+                        help='Comma-separated list of classification problems.')
 
     parser.add_argument('-flags', action='store', dest='flags', type=str,
                         default='weight_parents',
-                        type=str, help='Comma-separated list of flags to turn on/off '
+                        help='Comma-separated list of flags to turn on/off '
                         'in comparison.')
 
-    parser.add_argument('-vals', action='store',dest ='vals',default=False,
+    parser.add_argument('-vals', action='store',dest ='vals',default=None,type=str,
                         help='Specify values to compare in range as follows: '
                         'name:val1,val2,val3')
 
@@ -44,47 +52,63 @@ def main():
     args = parser.parse_args()
 
     # set values
-    estimators = ','.split(args.est)
-    classifiers = ','.split(args.clf)
-    reg_problems = ','.split(args.reg_problems)
-    clf_problems = ','.split(args.clf_problems)
-    flags = ','.split(args.flags)
-    #vals = ':'.split[0]:':'.split[1].
+    regressors = args.est.split(',')
+    classifiers = args.clf.split(',')
+    reg_problems = args.reg_problems.split(',')
+    clf_problems = args.clf_problems.split(',')
+    flags = args.flags.split(',')
+    if flags:
+        flags = ['--'+f for f in flags]
+        flags += ['']
+    print('flags: ',flags)
+    if args.vals:
+        vals = (args.vals.split(':')[0],args.vals.split(':')[1])
     n_cores = args.n_trials
     n_trials = args.n_trials
     data_dir = args.data_dir
     results_path = args.save_dir
 
-    for p in problems:
+    save_file = results_path + 'FEW-config-compare-' + '-'.join(flags) + '.csv'
+    print('save_file:',save_file)
+    # write header
+    with open(save_file,'w') as out:
+        out.write('dataset\tmethod\ttrial\tparameters\tscore\ttime\n')
+
+    for p in reg_problems:
         print(p)
-        # dataset = fetch_data(p,local_cache_dir=data_dir)
-    # for dataset in glob('/home/lacava/data/regression/*.txt'):
         dataset_name = data_dir + p + '.csv'
-        results_path = '/home/lacava/results/maize/'
 
-        for ml in estimators:
+        for ml in regressors:
             print('\t',ml)
-            for i in range(n_trials):
+            for flag in flags:
+                print('flag: ',flag)
+                method = flag.replace('--', '')
+                if flag=='':
+                    method = 'control'
 
-                job_name = ml + '_' + p + '_' + str(i)
-                save_file = results_path + ml + '_' + p + '.csv'
-                out_file = results_path + '{JOB_NAME}_%J.out'.format(JOB_NAME=job_name)
-                error_file = out_file.split('.')[0] + '.err'
-                #write header
-                with open(save_file,'w') as out:
-                    out.write('dataset\tmethod\ttrial\tparameters\tr2\ttime\n')
-                #submit job
-                bjob_line = ('bsub -o {OUT_FILE} -e {ERROR_FILE} -n {N_CORES} '
-                             '-J {JOB_NAME} -R "span[hosts=1]" '
-                             '"python {ML}.py {DATASET} {SAVE_FILE} {TRIAL} '
-                             '{N_CORES}"'.format(OUT_FILE=out_file,
-                                                 ERROR_FILE=error_file,
-                                                 JOB_NAME=job_name,
-                                                 N_CORES=n_cores,
-                                                 ML=ml,
-                                                 DATASET=dataset_name,
-                                                 SAVE_FILE=save_file,
-                                                 TRIAL=i,
-                                                 NCORES=n_cores))
-                # print(bjob_line)
-                os.system(bjob_line)
+                for i in range(n_trials):
+                    job_name = 'FEW-' + ml + '_' + p + '_' + str(i)
+                    save_file = results_path + 'FEW-' + ml + '_' + p + '.csv'
+                    out_file = results_path + '{JOB_NAME}_%J.out'.format(JOB_NAME=job_name)
+                    error_file = out_file.split('.')[0] + '.err'
+
+                    ################################################# submit job with flag
+                    bjob_line = ('bsub -o {OUT_FILE} -e {ERROR_FILE} -n {N_CORES} '
+                                 '-J {JOB_NAME} -R "span[hosts=1]" '
+                                 '"python FEW_job.py {DATASET} {SAVE_FILE} {TRIAL} '
+                                 '-method {METHOD} -ml {ML} {FLAG}"'.format(
+                                                                    OUT_FILE=out_file,
+                                                                    ERROR_FILE=error_file,
+                                                                    N_CORES=n_cores,
+                                                                    JOB_NAME=job_name,
+                                                                    DATASET=dataset_name,
+                                                                    SAVE_FILE=save_file,
+                                                                    TRIAL=i,
+                                                                    METHOD=method,
+                                                                    ML=ml,
+                                                                    FLAG=flag))
+                    # print(bjob_line)
+                    os.system(bjob_line)
+
+if __name__ == '__main__':
+    main()
